d60d944032b53cc605f115c6107a3e0e403704ec
Jason Lee <92654901+JasonLee0112@users.noreply.github.com>
2023-11-17 02:33:58 -0500

    Headers and Footers attached
    Each page has a header and a footer now

--- webpages/forum.php
+++ webpages/forum.php
@@ -1,2 +1,53 @@
 <!DOCTYPE html>
-<p> Forum </p>
+<html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    </head>
+    <body>
+        <!-- Header -->
+        <div class="navbar navbar-expand-lg bg-body-secondary sticky-top">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="/"> Pokedex+ </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" 
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"> Hi </span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="/">Home</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="pokedex">Pokedex</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="create.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Create
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="team">Team</a></li>
+                                <li><a class="dropdown-item" href="pokemon">Pokemon</a></li>
+                            </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="forum">Forum</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="justify-content-end nav-item">
+                        <a class="nav-link button" href="login">Login</a>
+                    </div>
+                </div>
+            </div>
+        <!-- End Header -->
+
+        <!-- Footer -->
+        <div class="navbar fixed-bottom bg-body-secondary">
+            <div class="container-fluid">
+                Footer
+            </div>
+        </div>
+        <!-- End Footer -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    </body>
+</html>
--- webpages/index.php
+++ webpages/index.php
@@ -29,55 +29,62 @@
     <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     </head>
-    <div class="navbar navbar-expand-lg bg-body-secondary sticky-top">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#"> Pokedex+ </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" 
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"> Hi </span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="pokedex">Pokedex</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="create.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Create
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="team">Team</a></li>
-                        <li><a class="dropdown-item" href="pokemon">Pokemon</a></li>
+    <body>
+        <!-- Header -->
+        <div class="navbar navbar-expand-lg bg-body-secondary sticky-top">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/"> Pokedex+ </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" 
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"> Hi </span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="pokedex">Pokedex</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="create.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Create
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="team">Team</a></li>
+                            <li><a class="dropdown-item" href="pokemon">Pokemon</a></li>
+                        </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="forum">Forum</a>
+                        </li>
                     </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="forum">Forum</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="justify-content-end nav-item">
-                <a class="nav-link button" href="login">Login</a>
+                </div>
+                <div class="justify-content-end nav-item">
+                    <a class="nav-link button" href="login">Login</a>
+                </div>
             </div>
         </div>
-    </div>
-        
-    <div>
-        <p> Pokemon </p>
+        <!-- End Header -->
 
-        <form method="POST">
-            
-            <input class="btn btn-primary" type="submit" value="Submit">
-        </form>
-    </div>
+        <!-- Content -->
+        <div>
+            <p> Pokemon </p>
 
-
-    <div class="navbar fixed-bottom bg-body-secondary">
-        <div class="container-fluid">
-            Footer
+            <form method="POST">
+                
+                <input class="btn btn-primary" type="submit" value="Submit">
+            </form>
         </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <!-- End Content -->
+
+        <!-- Footer -->
+        <div class="navbar fixed-bottom bg-body-secondary">
+            <div class="container-fluid">
+                Footer
+            </div>
+        </div>
+        <!-- End Footer -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    </body>
 </html>
