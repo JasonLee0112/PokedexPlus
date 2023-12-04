2f7bdf56cec6bcccdd02043e022213dc36370ff0
RichyKim12 <109635077+RichyKim12@users.noreply.github.com>
2023-12-04 09:10:56 -0500

    small signin/up page fix get rid of Google

--- webpages/accountloginsignup.php
+++ webpages/accountloginsignup.php
@@ -3,8 +3,8 @@
     <head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="styles.css">
-        <script src="https://apis.google.com/js/platform.js" async defer></script>
-        <meta name="google-signin-client_id" content="926122338315-95c1lbkulm3en0ui5icuechntk4eq7jn.apps.googleusercontent.com">
+        <!-- <script src="https://apis.google.com/js/platform.js" async defer></script> -->
+        <!-- <meta name="google-signin-client_id" content="926122338315-95c1lbkulm3en0ui5icuechntk4eq7jn.apps.googleusercontent.com"> -->
     </head>
     <body>
         <?php
@@ -48,7 +48,7 @@
 
         </script>
         <div class="g-signin2" data-onsuccess="onSignIn"></div>
-        <button onclick="signOut()">Sign out</button>
+        
 
         <button onclick="signUp()"> Sign Up</button>
         <button onclick="signIn()"> Sign In</button>
