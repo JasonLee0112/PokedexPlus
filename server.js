eae8e231287a042d472b1d21bae00b46a6e883d3
RichyKim12 <109635077+RichyKim12@users.noreply.github.com>
2023-12-04 05:43:43 -0500

    commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const path = require('path')
-
+const session = require('express-session');
 const express = require('express');
 const { spawn } = require('child_process');
 const app = express();
@@ -12,7 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
+app.use(session({
+    secret: 'not-secure-key', // Change this to a secure secret key
+    resave: false,
+    saveUninitialized: true
+  }));
 
 app.get('/sign-up', (req, res) => {
   
@@ -210,30 +214,30 @@ app.get('/sign-in', (req,res) => {
         res.end();
     });
 })
-app.get('/test', (req,res)=> {
-    console.log('test log');
-    let scriptPath ="./webpages/test.php";
-    const phpProcess = spawn('php',[scriptPath]);
-    let authenticateResult= "";
+app.get('/test', (req, res) => {
+    let scriptPath = "./webpages/test.php";
+    const phpProcess = spawn('php', [scriptPath]);
+    let authenticateResult = "";
+
     phpProcess.stdout.on('data', (data) => {
         const output = data.toString().trim();
         authenticateResult += output;
-        console.log(authenticateResult);
+        
     });
-    res.end();
-})
-app.get('/shutdown', (req,res)=> {
-    console.log('test log');
-    let scriptPath ="./webpages/shutdown.php";
-    const phpProcess = spawn('php',[scriptPath]);
-    let authenticateResult= "";
-    phpProcess.stdout.on('data', (data) => {
-        const output = data.toString().trim();
-        authenticateResult += output;
-        console.log(authenticateResult);
+
+    phpProcess.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
     });
-    res.end();
-})
+
+    phpProcess.on('close', (code) => {
+        console.log("test", authenticateResult);
+        res.end();
+        // console.log(`child process exited with code ${code}`);
+        // res.send(authenticateResult);  // Send the response after the PHP script has completed
+    });
+});
+
+
 app.post('/authenticate', (req,res) => {
     let scriptPath = "./webpages/authenticate.php";
     const username = req.body.username;
@@ -245,19 +249,28 @@ app.post('/authenticate', (req,res) => {
     phpProcess.stdout.on('data', (data) => {
         const output = data.toString().trim();
         authenticateResult += output;
-        console.log(authenticateResult);
         if(authenticateResult.includes("Invalid username, email, or password")){
-            console.log("authentication failed")
+            // console.log("authentication failed")
             res.redirect("/sign-in");
+            return
         }
         if(authenticateResult.includes("Logged In")){
-            console.log("authentication success")
-            res.redirect("/test");
+            // console.log("authentication success")
+            
+            console.log("auth", authenticateResult);
+            // res.end();
+            // res.redirect("/test");
+            return
         }
+        
     });
     phpProcess.stderr.on('data', (data) => {
         console.error(`stderr: ${data}`);
     });
+    // phpProcess.on('close', (code) => {
+    //     console.log("auth", authenticateResult);
+    //     res.redirect("/home");  // Send the response after the PHP script has completed
+    // });
    
 })
 
--- webpages/test.php
+++ webpages/test.php
@@ -1,12 +1,14 @@
 <?php
-    session_start();
-    $userID = $_SESSION['user_id'];
-    echo 'UserID: ';
-    echo $userID;
-    if (isset($_SESSION['user_id'])) {
-        echo $_SESSION['user_id'];
-    } 
-    else {
-        echo 'user_id not set';
-    }
+session_start();
+echo session_save_path();
+// echo session_id();
+// $userID = $_SESSION['user_id'];
+// echo 'UserID: ';
+// echo $userID;
+if (isset($_SESSION['user_id'])) {
+    // echo $_SESSION['user_id'];
+} 
+else {
+    // echo 'user_id not set';
+}
 ?>
